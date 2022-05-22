f0df97b84be47222dd6a11b1d996dc5ed5d4b5f0
Sadna87 <105720652+Sadna87@users.noreply.github.com>
2022-05-22 16:26:39 +0300

    Add files via upload

---
+++ remedies/Anis.php
@@ -0,0 +1,426 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>Granny's remedies</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="Free HTML Templates" name="keywords">
+    <meta content="Free HTML Templates" name="description">
+
+    <!-- Favicon -->
+    <link href="../img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat&family=Oswald:wght@400;500;600&display=swap" rel="stylesheet"> 
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Flaticon Font -->
+    <link href="../lib/flaticon/font/flaticon.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="../lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <link href="../lib/lightbox/css/lightbox.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="../css/style.css" rel="stylesheet">
+	
+	
+	
+
+</head>
+
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid bg-dark py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center text-lg-left mb-2 mb-lg-0">
+                    
+                </div>
+                <div class="col-md-6 text-center text-lg-right">
+                    <div class="d-inline-flex align-items-center">
+                        <a class="text-white px-3" href="facebook.html">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                        <a class="text-white px-3" href="twitter.html">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                        
+                        <a class="text-white px-3" href="instegram.html">
+                            <i class="fab fa-instagram"></i>
+                        </a>
+                        <a class="text-white pl-3" href="youtube.html">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid position-relative nav-bar p-0">
+        <div class="container position-relative" style="z-index: 9;">
+            <nav class="navbar navbar-expand-lg bg-secondary navbar-dark py-3 py-lg-0 pl-3 pl-lg-5">
+			<a class="navbar-brand " href="../index.html">
+           <img src="../img\grandma.jpg" class="logo">
+        </a>
+
+                <a href="../index.html" class="navbar-brand">
+                    <h1 class="m-0 display-5 text-white font">Grandma's Remedies</h1>
+                </a>
+                <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-between px-3" id="navbarCollapse">
+                    <div class="navbar-nav ml-auto py-0">
+                        <a href="../index.html" class="nav-item nav-link">Home</a>
+                        <a href="../about.html" class="nav-item nav-link">About</a>
+                        <a href="../bot.html" class="nav-item nav-link">Chatbot</a>
+                        
+                        <div class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle active" data-toggle="dropdown">Symptoms</a>
+                            <div class="dropdown-menu rounded-0 m-0">
+                                <a href="Ears.html" class="dropdown-item">Ears Pain</a>
+                                <a href="Head.html" class="dropdown-item">Head Ache </a>
+								<a href="Skin.html" class="dropdown-item">Skin Care</a>
+                                <a href="Hair.html" class="dropdown-item">Hair Care</a>
+								<a href="Throat.html" class="dropdown-item">Throat Pain</a>
+                                <a href="Stomach.html" class="dropdown-item">Stomach Ache</a>
+								<a href="Fever.html" class="dropdown-item">Fever</a>
+                                <a href="Cold.html" class="dropdown-item">Cold Symptoms</a>
+								<a href="LDH.html" class="dropdown-item">High LDH</a>
+                                <a href="Cholesterol.html" class="dropdown-item">High Cholesterol</a>
+								<a href="Coughing.html" class="dropdown-item">Coughing</a>
+                                <a href="Vomiting.html" class="dropdown-item">Nausea and Vomiting</a>
+                            </div>
+                        </div>
+                        <a href="../contact.html" class="nav-item nav-link">Contact</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- Navbar End -->
+
+
+    <!-- Under Nav Start -->
+    <div class="container-fluid bg-white py-3">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-4 text-left mb-3 mb-lg-0">
+                    <div class="d-inline-flex text-left">
+                        <h1 class="flaticon-office font-weight-normal text-primary m-0 mr-3"></h1>
+                        <div class="d-flex flex-column">
+                            <h5>Our Office</h5>
+                            <p class="m-0">Bialik 23, Tel-aviv, ISRAEL</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-left text-lg-center mb-3 mb-lg-0">
+                    <div class="d-inline-flex text-left">
+                        <h1 class="flaticon-email font-weight-normal text-primary m-0 mr-3"></h1>
+                        <div class="d-flex flex-column">
+                            <h5>Email Us</h5>
+                            <p class="m-0">Homeremedy@gmail.com</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 text-left text-lg-right mb-3 mb-lg-0">
+                    <div class="d-inline-flex text-left">
+                        <h1 class="flaticon-telephone font-weight-normal text-primary m-0 mr-3"></h1>
+                        <div class="d-flex flex-column">
+                            <h5>Call Us</h5>
+                            <p class="m-0">08-6300012</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Under Nav End -->
+
+
+    
+
+
+    <!-- Detail Start -->
+    <div class="container py-5">
+        <div class="row pt-5">
+            <div class="col-lg-8">
+                <div class="d-flex flex-column text-left mb-4">
+                   
+                    <h1 class="mb-4 section-title">Anise</h1>
+
+                </div>
+                <p>Anise seed is a powerful plant that is rich in many nutrients and boasts a wide array of health benefits.
+          It has anti-fungal, antibacterial and anti-inflammatory properties and may fight stomach ulcers, keep blood sugar levels in check and reduce symptoms of depression and menopause.
+          Combined with a nutritious diet and healthy lifestyle, anise seed could improve several aspects of your health. </p>
+				
+                <div class="mb-5">
+                    <img class="img-fluid w-100 mb-4" src="../img/Anis.jpg" alt="Image">
+                  
+					    <p><h2>how to use</h2>
+					in traditional Chinese and folk medicine practices, star anise is steeped in water to make a tea used to treat respiratory infections, nausea, constipation and other digestive issues. Star anise also makes a great addition to sweet dishes and desserts, such as baked fruit, pies, quick bread and muffins<br>
+                         <h2>Benefits</h2>
+					    -upset stomach<br>
+                        -runny nose.<br>
+						-as an expectorant to increase productive cough<br>
+					    -as a diuretic to increase urine flow.<br>
+                        -as an appetite stimulant.<br>
+
+						</p>
+		
+ 
+              
+                </div>
+ <!-- התמונות הזזות למטה -->
+                 <div class="mb-5 mx-n3">
+                    <h3 class="mb-4 ml-3 section-title">Related Post</h3>
+                    <div class="owl-carousel service-carousel position-relative">
+                        <div class="card border-0 mx-3">
+                            <img class="card-img-top" src="../img/Mentha.jpg" alt="">
+                            <div class="card-body bg-light p-4">
+                                <div class="d-flex align-items-center mb-3">
+                                    <a class="btn btn-primary" href="Mint.php"><i class="fa fa-link"></i></a>
+                                    <h5 class="m-0 ml-3 text-truncate">Mentha</h5>
+                                </div>
+                                <p>mentha is a genus of plants in the family Lamiaceae (mint family).</p>
+                               
+                            </div>
+                        </div>
+                        <div class="card border-0 mx-3">
+                            <img class="card-img-top" src="../img/Honey.jpg" alt="">
+                            <div class="card-body bg-light p-4">
+                                <div class="d-flex align-items-center mb-3">
+                                    <a class="btn btn-primary" href="Honey.php"><i class="fa fa-link"></i></a>
+                                    <h5 class="m-0 ml-3 text-truncate">Honey</h5>
+                                </div>
+                                <p>Honey is a syrupy liquid that honeybees make from plant nectar. Loved worldwide for its sweetness and depth of flavor, it’s used in many foods and recipes.</p>
+                               
+                            </div>
+                        </div>
+                        <div class="card border-0 mx-3">
+                            <img class="card-img-top" src="../img/Garlic.jpg" alt="">
+                            <div class="card-body bg-light p-4">
+                                <div class="d-flex align-items-center mb-3">
+                                    <a class="btn btn-primary" href="Garlic.php"><i class="fa fa-link"></i></a>
+                                    <h5 class="m-0 ml-3 text-truncate">Garlic</h5>
+                                </div>
+                                <p>garlic is traditionally used to treat colds and coughs. It's also reported to boost the immune system and help ease asthma symptoms. Arab traditional medicine recommended garlic to help treat heart disease, high blood pressure, arthritis, toothache, constipation, and infections</p>
+                               
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                
+ <!-- התמונות הזזות למטה -->
+ 
+ <form name="MyForm" action="" method="post"  autocomplete="on">
+                <div class="bg-light p-5">
+                    <h3 class="mb-4 section-title">Leave a comment</h3>
+                    <form>
+                        <div class="form-group">
+                            <label for="name">Name *</label>
+                            <input type="text" class="form-control" id="name" name="name">
+                        </div>
+                        
+                       
+
+                        <div class="form-group">
+                            <label for="message">Message *</label>
+                            <textarea id="message" name="message" cols="30" rows="5" class="form-control"></textarea>
+                        </div>
+                          <input type="text" id="currentDate" name="date" hidden >
+                           <input type="text" id="currentTime" name="time" hidden>
+<p>Rate</p> 
+<div class="rate">
+    <input type="radio" id="star5" name="rate" value="&#11088;&#11088;&#11088;&#11088;&#11088" />
+    <label for="star5" title="text">5 stars</label>
+    <input type="radio" id="star4" name="rate" value="&#11088;&#11088;&#11088;&#11088" />
+    <label for="star4" title="text">4 stars</label>
+    <input type="radio" id="star3" name="rate" value="&#11088;&#11088;&#11088" />
+    <label for="star3" title="text">3 stars</label>
+    <input type="radio" id="star2" name="rate" value="&#11088;&#11088" />
+    <label for="star2" title="text">2 stars</label>
+    <input type="radio" id="star1" name="rate" value="&#11088" />
+    <label for="star1" title="text">1 star</label>
+  </div>
+<br>
+<br>
+
+                        <div class="form-group mb-0 ">
+                            <input type="submit" value="Leave Comment" name="post" class="btn btn-primary px-3 ">
+                        </div>
+                    </form>
+                </div>
+            </div>
+<form>
+
+<script>
+  var today = new Date();
+  var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+  document.getElementById("currentDate").value = date;
+  
+  var today = new Date();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+ document.getElementById("currentTime").value = time;
+  
+  
+</script>
+
+</html>
+<?php
+$name = $_POST["name"];
+$message = $_POST["message"];
+$post = $_POST["post"];
+$date = $_POST["date"];
+$time = $_POST["time"];
+$rate = $_POST["rate"];
+
+# write comments#
+if($post){
+$write = fopen("anis.txt", "a+");
+fwrite($write, "<u><b> $name</b></u><br>$message<br>$date<br>$time<br>$rate<br>");
+fclose($write);
+
+#display comments#
+$read = fopen("anis.txt", "r+t");
+echo "all comments:<br>";
+
+while(!feof($read)){
+	echo fread($read, 1024);
+}
+fclose($read);
+}
+else{
+	
+	#display comments#
+$read = fopen("anis.txt", "r+t");
+echo "all comments:<br>";
+
+while(!feof($read)){
+	echo fread($read, 1024);
+}
+fclose($read);	
+	
+	
+}
+
+$server_name = "localhost";
+$user_name = "yonatanp_user";
+$password = "12345";
+$database = "yonatanp_project";
+
+$conn = new mysqli($server_name, $user_name, $password, $database);
+if ($conn->connect_error) {
+    die("Error connecting: " . $conn->connect_error);
+}
+$sql = "select Name,Message from comments";
+$result = $conn->query($sql);
+#if ($result->num_rows > 0) {
+    /*echo    "
+	<table>
+	<tr>
+	 <th>name</th> 
+	  <th>message</th>
+	  
+	 
+	</tr>";
+    while ($row = $result->fetch_assoc()) {
+        echo "
+		<tr>
+		  <td>" . $row["Name"] . "</td>
+		  <td>". $row["Message"] . " </td>
+		  
+		  
+		</tr>" ;
+    }
+    echo "</table>";
+} else
+    echo "No data found";*/
+$name=$_POST['name'];
+$message=$_POST['message'];
+
+
+$sql="insert into comments(Name,Message) values ('".$name."','".$message."')";
+#echo $sql;
+if ($conn->query($sql)==FALSE){
+    echo "Can not add new user.  Error is: ".$conn->error;
+    exit();
+}
+
+
+
+
+
+
+
+?>
+
+<html>
+
+
+
+
+	  
+
+    <!-- Footer Start -->
+    <div class="container-fluid bg-dark text-white mt-5 py-5 px-sm-3 px-md-5">
+        <div class="row pt-5">
+            <div class="col-lg-12 col-md-12 mb-12">
+                <h4 class="text-primary mb-4">Get In Touch</h4>
+                <p><i class="fa fa-map-marker-alt mr-2"></i>Bialik 23, Tel-aviv, ISRAEL</p>
+                <p><i class="fa fa-phone-alt mr-2"></i>08-6300012</p>
+                <p><i class="fa fa-envelope mr-2"></i>Homeremedy@gmail.com</p>
+                
+            </div>
+            
+            <div class="col-lg-12 col-md-12 mb-12">
+                
+               <div class="d-flex flex-column justify-content-start">
+                    
+					<video class="videoFight" autoplay muted >
+  <source src="../img\remedy.mp4" type="video/mp4">
+
+					<video width="200" height="240" controls>
+  <source src="movie.mp4" type="video/mp4">
+  <source src="movie.ogg" type="video/ogg">
+Your browser does not support the video tag.
+</video>
+                </div>
+            </div>
+            
+        </div>
+       
+    </div>
+    <!-- Footer End -->
+
+
+    <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary back-to-top"><i class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="../lib/easing/easing.min.js"></script>
+    <script src="../lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="../lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="../lib/lightbox/js/lightbox.min.js"></script>
+
+    <!-- Contact Javascript File -->
+    <script src="../mail/jqBootstrapValidation.min.js"></script>
+    <script src="../mail/contact.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="../js/main.js"></script>
+</body>
+
+</html>
